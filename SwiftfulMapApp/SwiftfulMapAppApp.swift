7ed67399f2748c4ddbb2d3ab47453f5834af17b1
Juan David <juandaguti@icloud.com>
2025-06-26 11:12:13 +0100

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Juan David Gutierrez Olarte on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
