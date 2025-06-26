90d3890f742d7594dfb11cc12a1d351a82cd8f96
Juan David <juandaguti@icloud.com>
2025-06-26 11:34:20 +0100

    Assets and Project Setup. Model and Data Services setup

---
+++ SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by Juan David Gutierrez Olarte on 26/06/25.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
---
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -0,0 +1,22 @@
+//
+//  LocationsView.swift
+//  SwiftfulMapApp
+//
+//  Created by Juan David Gutierrez Olarte on 26/06/25.
+//
+
+import SwiftUI
+
+class LocationsViewModel: ObservableObject {
+    
+}
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
